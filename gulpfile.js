322cb4eee8641d012ab945532ea04c24735d69c2
yashimoto <wojsamjan@gmail.com>
2017-05-13 21:24:59 +0200

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require('gulp'),
-  connect = require('gulp-connect');
+var gulp = require('gulp');
+var connect = require('gulp-connect');
  
 gulp.task('connect', function() {
   connect.server({
